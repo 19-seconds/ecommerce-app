f5b4a7498045a21896ff70df583c9c2f0526c20c
Mark Richardson <markeban@yahoo.com>
2017-01-25 13:36:17 +0200

    dismissible alerts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,8 @@ class ProductsController < ApplicationController
       image: params[:image]
       })
     product.save
+    flash[:success] = "Product Created"
+    redirect_to "/products/#{product.id}"
   end
 
   def edit
@@ -27,14 +29,24 @@ class ProductsController < ApplicationController
   end
 
   def update
-    @product = Product.find_by(id: params[:id])
-    @product.assign_attributes({
+    product = Product.find_by(id: params[:id])
+    product.assign_attributes({
       name: params[:name],
       description: params[:description],
       price: params[:price],
       image: params[:image]
     })
-    @product.save
+    product.save
+    flash[:success] = "Product Updated"  
+    redirect_to "/products/#{product.id}"
+
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    flash[:danger] = "Product Deleted"
+    redirect_to "/products"
   end
 
 end
