065e4067c65a38461d0ccf672bada7d84a4eb0d4
Mark Richardson <markeban@yahoo.com>
2017-02-06 12:42:52 +0200

    current user can place orders

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,14 @@ Rails.application.routes.draw do
   get "/products/:id/edit", to: 'products#edit'
   patch "/products/:id", to: 'products#update'
   delete "/products/:id", to: 'products#destroy'
+
+  get "/signup", to: "users#new"
+  post "/users", to: "users#create"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/orders", to: 'orders#create'
+  get "/orders/:id", to: 'orders#show'
 end
