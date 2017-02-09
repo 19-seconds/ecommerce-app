93c340f040ffaf9c91d2494d0c33ed36b415c87c
Mark Richardson <markeban@yahoo.com>
2017-02-09 11:10:34 +0200

    shopping cart now exists

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,20 +1,12 @@
 class CartedProductsController < ApplicationController
 
   def index
-    
+    @carted_products = current_user.orders.find_by(completed: false).carted_products
   end
 
 
   def create
-    # Check the orders table for any orders that belong to the current_user and has an order that's open (aka completed: false)
-    orders = Order.where("user_id = ? AND completed = ?", current_user.id, false)
-    if orders.first
-      order = orders.first
-    else
-      order = Order.new(user_id: current_user.id, completed: false)
-      order.save
-    end
-
+    order = current_user.orders.find_by(completed:false) || Order.create(user_id: current_user.id, completed: false)
     carted_product = CartedProduct.new(order_id: order.id, product_id: params[:product_id], quantity: params[:quantity])
     carted_product.save
     redirect_to "/carted_products"
